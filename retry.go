0941d91818244749d0d10b2604c5079df9d35abe
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-03-19 21:09:21 -0700

    reorder and document

--- retry.go
+++ retry.go
@@ -10,6 +10,50 @@ import (
 	"time"
 )
 
+// the retry interface facilitates signaling the entrance of a state after
+// an arbitrary amount of time.
+type Interface interface {
+	// returns a channel which, on receiving a value, notifies the caller
+	// that an action should be retried.
+	Retry() <-chan time.Time
+}
+
+// the returned interface sends after initial nanos on the first call to Retry().
+// successed calls to the interface's Retry() method signal in delay(initial)
+// nanos, delay(delay(initial)) nanos, ...
+func Retry(initial time.Duration, delay ...DelayFunc) Interface {
+	checkRollover(initial)
+	if len(delay) == 0 {
+		return retry{initial, nil}
+	}
+	if len(delay) == 1 {
+		return retry{initial, delay[0]}
+	}
+	return retry{initial, Delay(delay...)}
+}
+
+// if n is less than 1 c is closed immediately.
+// if r is nil it is equivalent to passing Retry(0).
+func MaxTries(n int, r Interface) (Interface, <-chan struct{}) {
+	c := make(chan struct{})
+	_r := &maxretries{
+		c: c,
+		n: n,
+		r: r,
+	}
+	if n < 1 {
+		close(c)
+	}
+	return _r, c
+}
+
+// an iterative function for computing retry delays. delays are computed as
+//	d1 = f(d0)
+//	d2 = f(d1)
+//	d3 = f(d2)
+//	...
+type DelayFunc func(time.Duration) time.Duration
+
 // chain fns together to construct a new DelayFunc. Delay(f, g, h)(d) is equivalent to h(g(f(d))).
 func Delay(fns ...DelayFunc) DelayFunc {
 	return func(d time.Duration) time.Duration {
@@ -25,7 +69,7 @@ func Delay(fns ...DelayFunc) DelayFunc {
 //	[d - plusminus, d + plusminus]
 func Randomize(plusminus time.Duration) DelayFunc {
 	checkRollover(plusminus)
-	size := int64(2 * plusminus + 1)
+	size := int64(2*plusminus + 1)
 	sub := plusminus
 	return func(d time.Duration) time.Duration {
 		offset := time.Duration(rand.Int63n(size)) - sub
@@ -33,6 +77,8 @@ func Randomize(plusminus time.Duration) DelayFunc {
 	}
 }
 
+// the returned DelayFunc passes arguments through, but returns max if the
+// argument is greater than max. panics if max is negative.
 func BoundedMax(max time.Duration) DelayFunc {
 	if max < 0 {
 		panic("negative bound")
@@ -45,6 +91,8 @@ func BoundedMax(max time.Duration) DelayFunc {
 	}
 }
 
+// the returned DelayFunc passes arguments through, but returns min if the
+// argument is greater than max. panics if max is negative.
 func BoundedMin(min time.Duration) DelayFunc {
 	if min < 0 {
 		panic("negative bound")
@@ -57,6 +105,8 @@ func BoundedMin(min time.Duration) DelayFunc {
 	}
 }
 
+// panics if max is less than min. otherwise functionally equivalent to
+//	Delay(BoundedMin(min), BoundedMax(max))
 func Bounded(min, max time.Duration) DelayFunc {
 	if max < min {
 		panic("undefined bounds")
@@ -67,29 +117,14 @@ func Bounded(min, max time.Duration) DelayFunc {
 	return Delay(BoundedMin(min), BoundedMax(max))
 }
 
-// an iterative function for computing retry delays. delays are computed as
-//	d1 = f(d0)
-//	d2 = f(d1)
-//	d3 = f(d2)
-//	...
-type DelayFunc func(time.Duration) time.Duration
-
-func checkRollover(d time.Duration) {
-	if d < 0 {
-		panic("negative duration")
-	}
-}
-
+// returns a DelayFunc that always produces delay.
 func Constant(delay time.Duration) DelayFunc {
 	return func(time.Duration) time.Duration { return delay }
 }
 
-func exponential2(d time.Duration) time.Duration {
-	d = d << 1
-	checkRollover(d)
-	return d
-}
-
+// a DelayFunc that multiplies by scale on application.
+// iterative application produces growth on the order of
+//	scale**n
 func Exponential(scale float64) DelayFunc {
 	if scale < 0 {
 		panic("negative scale")
@@ -107,19 +142,16 @@ func Exponential(scale float64) DelayFunc {
 	}
 }
 
-// if n is less than 1 c is closed immediately.
-// if r is nil it is equivalent to passing Retry(0).
-func MaxTries(n int, r Interface) (Interface, <-chan struct{}) {
-	c := make(chan struct{})
-	_r := &maxretries{
-		c: c,
-		n: n,
-		r: r,
+func checkRollover(d time.Duration) {
+	if d < 0 {
+		panic("negative duration")
 	}
-	if n < 1 {
-		close(c)
-	}
-	return _r, c
+}
+
+func exponential2(d time.Duration) time.Duration {
+	d = d << 1
+	checkRollover(d)
+	return d
 }
 
 type maxretries struct {
@@ -152,21 +184,4 @@ func (r retry) Retry() <-chan time.Time {
 		r.d = r.delay(r.d)
 	}
 	return c
-}
-
-type Interface interface {
-	Retry() <-chan time.Time
-}
-
-// The Retry() on the returned interface receives a value after the initial duration.
-// The initial duration seeds iterative calles to delay on successive calls to Retry()
-func Retry(initial time.Duration, delay ...DelayFunc) Interface {
-	checkRollover(initial)
-	if len(delay) == 0 {
-		return retry{initial, nil}
-	}
-	if len(delay) == 1 {
-		return retry{initial, delay[0]}
-	}
-	return retry{initial, Delay(delay...)}
 }
